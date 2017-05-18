37da9616202136e59b4bbb9c2fa80c48ddd552e5
Eduardo Orellana Ramirez <Eduardo Orellana Ramirez>
2017-05-18 18:58:59 -0400

    el controlador denuevo

--- app/Http/Controllers/EncuestaController.php
+++ app/Http/Controllers/EncuestaController.php
@@ -4,41 +4,75 @@ namespace App\Http\Controllers;
 
 use View;
 
-use App\Perfil;
-use App\User;
-
 use Illuminate\Http\Request;
 
-class UsuarioController extends Controller
+class EncuestaController extends Controller
 {
-    public function IngresoProfesional()
-    {
-      $datos = Perfil::TiposPerfiles();
-      return View::make('CrearProfesional.IngresoProfesional')->with("datos",$datos );
-    }
+   
 
-    public function CrearProfesional()
+    public function IngresarEncuesta()
     {
         $this->validate(request(), [
             'Nombre' => ['required', 'max:200'],
-            'Apellidos' => ['required', 'max:200'],
             'Rut' => ['required', 'max:200'],
-            'Profesion' => ['required', 'max:200'],
-            'Email' => ['required', 'max:200'],
-            'Password' => ['required', 'max:200']
-
+            'Fnacimiento' => ['required', 'max:200'],
+            'Edad' => ['required', 'max:200'],
+            'Escolaridad' => ['required', 'max:200'],
+            'Nhermanos' => ['required', 'max:200'],
+            'Phermanos' => ['required', 'max:200'],
+            'NomPadre'=>['required','max:200'],
+            'NomMadre'=>['required','max:200'],
+            'Direccion'=>['required','max:200'],
+            'Telefono'=>['required','max:200'],
+            'CorrElec'=>['required','max:200'],
+            'NomCompFicha/Fecha'=>['required','max:200'],
+            'p21'=>['required','max:200'],
+            'p22'=>['required','max:200'],
+            'p23'=>['required','max:200'],
+            'p24'=>['required','max:200'],
+            'p25'=>['required','max:200'],
+            'p31'=>['required','max:200'],
+            'p32'=>['required','max:200'],
+            'p41'=>['required','max:200'],
+            'p42'=>['required','max:200'],
+            'p43'=>['required','max:200'],
+            'p44'=>['required','max:200'],
+            'p45'=>['required','max:200'],
+            'p51'=>['required','max:200'],
+            'p52'=>['required','max:200'],
+            'p53'=>['required','max:200'],
+            'p54'=>['required','max:200'],
+            'p55'=>['required','max:200'],
+            'p56'=>['required','max:200'],
+            'p57'=>['required','max:200'],
+            'p58'=>['required','max:200'],
+            'p59'=>['required','max:200'],
+            'p510'=>['required','max:200'],
+            'p61'=>['required','max:200'],
+            'p62'=>['required','max:200'],
+            'p63'=>['required','max:200'],
+            'p64'=>['required','max:200'],
+            'p65'=>['required','max:200'],
+            'p66'=>['required','max:200'],
+            'p71'=>['required','max:200'],
+            'p72'=>['required','max:200'],
+            'p73'=>['required','max:200'],
+            'p74'=>['required','max:200'],
+            'p75'=>['required','max:200'],
+            'p76'=>['required','max:200'],
+      
         ]);
 
       $data = request()->all();
 
-      $resultado = User::Agregar($data['Nombre'],$data['Apellidos'],
+      /*$resultado = User::Agregar($data['Nombre'],$data['Apellidos'],
                                   $data['Rut'],$data['Profesion'],$data['Email'],$data['Password']);
 
       if ($resultado == true)
       {
         return redirect()->to('Mi_menu');
       }
-      else echo "ya existe usuario";
+      else echo "ya existe usuario";*/
 
     }
 
